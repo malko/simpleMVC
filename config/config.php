cfc35c937c5ee9863ca6dea30886d34005c39557
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2008-06-16 14:38:02 +0000

    - add sample config file
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@22 f9b170eb-6430-4d69-81fd-c2540bca983d

---
+++ config/config.php
@@ -0,0 +1,11 @@
+#- <?php echo "Access Denied!"; exit(); ?>
+#- GENERAL CONFIG FOR APPS
+#- ADD ANY CONSTANTS YOU MAY REQUIRE HERE
+
+#- DATABASE CONNECTION
+DB_CONNECTION  = mysqldb://dbname;dbhost;dbuser;dbpass
+
+#- SOME PATH CONFIGURATION REQUIRED OR USEFULL FOR FURTHER DEVELOPPMENT.
+ROOT_URL     = http://example.com/
+APP_URL      = %ROOT_URL%/%FRONT_NAME%
+APP_DIR      = %ROOT_DIR%/%FRONT_NAME%
