4e2618f79fb45ffacdd002082d38f87116ca665d
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2008-08-08 11:53:46 +0000

    - use langMangager
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@27 f9b170eb-6430-4d69-81fd-c2540bca983d

--- includes/views/models_list.tpl.php
+++ includes/views/models_list.tpl.php
@@ -2,7 +2,7 @@
 	echo "<h1>$this->pageTitle</h1>";
 	echo '
 	<div style="text-align:right;">
-		<a href="'.$this->url('add',null,array('modelType'=>$this->modelType)).'"><img src="'.ELEMENTS_URL.'/icones/admin/bouton_ajouter_icone.png" alt="ajouter un &eacute;lement"/> Ajouter un &eacute;lement.</a>
+		<a href="'.$this->url('add',null,array('modelType'=>$this->modelType)).'"><img src="'.ELEMENTS_URL.'/icones/admin/add.png" alt="'.langManager::msg('Add new item').'"/> '.langManager::msg('Add new item').'.</a>
 	</div>';
 	echo $this->adminSortableList($this->listDatas,$this->listHeaders);
 ?>
