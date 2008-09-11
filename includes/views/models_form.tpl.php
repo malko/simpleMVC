f1bb4efa11d91485381a90bb5369f3fd0f8181cc
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2008-09-11 14:52:19 +0000

    - change layout
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@37 f9b170eb-6430-4d69-81fd-c2540bca983d

--- includes/views/models_form.tpl.php
+++ includes/views/models_form.tpl.php
@@ -1,10 +1,12 @@
 <h1><?= $this->pageTitle ?></h1>
+<table border=1>
 <?php
 	if( !empty($this->datasDefs) ){
 		foreach($this->datasDefs as $k=>$d){
-			echo $this->modelFormInput(isset($this->_model_)?$this->_model_:$this->modelType,$k);
+			echo $this->modelFormInput(isset($this->_model_)?$this->_model_:$this->modelType,$k,array('formatStr'=>'<tr><td>%label</td><td>%input</td></tr>'));
 		}
 	}
 ?>
+</table>
 <input type="reset" onclick="window.location = '<?= $this->listUrl ?>';"; value="<?= langManager::msg('back'); ?>"  class="noSize"/>
 <input type="submit" value="<?= langManager::msg('save'); ?>" class="noSize" />
