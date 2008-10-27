3f796aa41b8cace13daf6067fcccd1b8d5de1ab4
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2008-10-27 14:14:08 +0000

    - add form tags - now will check for inputOpts inside view vars to ovveride default behaviour for each fields.
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@48 f9b170eb-6430-4d69-81fd-c2540bca983d

--- includes/views/models_form.tpl.php
+++ includes/views/models_form.tpl.php
@@ -1,12 +1,23 @@
 <h1><?= $this->pageTitle ?></h1>
-<table border=1>
+<form action="<?= $this->url('save') ?>" method="post">
+<table border="0" cellspacing="0" cellpadding="2">
 <?php
 	if( !empty($this->datasDefs) ){
+		$inputOpts = array(
+			'formatStr'=>'<tr class="formInput"><td>%label</td><td>%input</td></tr>'
+		);
 		foreach($this->datasDefs as $k=>$d){
-			echo $this->modelFormInput(isset($this->_model_)?$this->_model_:$this->modelType,$k,array('formatStr'=>'<tr><td>%label</td><td>%input</td></tr>'));
+			$opts = $inputOpts;
+			if( isset($this->inputOpts[$k] ) )
+			$opts = array_merge($inputOpts,$this->inputOpts[$k]);
+			if((! isset($this->_model_)) && !empty($this->{$k})){
+				$opts['value'] = $this->{$k};
+			}
+			echo $this->modelFormInput(isset($this->_model_)?$this->_model_:$this->modelType,$k,$opts);
 		}
 	}
 ?>
 </table>
 <input type="reset" onclick="window.location = '<?= $this->listUrl ?>';"; value="<?= langManager::msg('back'); ?>"  class="noSize"/>
 <input type="submit" value="<?= langManager::msg('save'); ?>" class="noSize" />
+</form>
