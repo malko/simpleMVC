e0df0575265698fa0d9c55b858c07bbfa844380c
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2009-03-27 13:58:21 +0000

    - updated to use latest jqueryUI
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@107 f9b170eb-6430-4d69-81fd-c2540bca983d

--- includes/views/helpers/datepicker.php
+++ includes/views/helpers/datepicker.php
@@ -3,7 +3,8 @@
 * helper to easily incorporate jquery Datepicker
 * @package simpleMVC
 * http://marcgrabanski.com/code/ui-datepicker
-* @changelog 
+* @changelog
+*            - 2009-03-27 - update to work with last version of jqueryUI (1.7)
 *            - 2009-01-05 - add withTime method to combine datepicker and timepicker (use new timepicker helper)
 *            - 2008-11-07 - change _js_script call (not static anymore)
 */
@@ -11,33 +12,42 @@
 class datepicker_viewHelper extends  jsPlugin_viewHelper{
 	/** path relative to jQuery_viewHelper::$pluginPath/$pluginName */
 	public $requiredFiles = array(
-		'js/jqueryPlugins/jqueryDatepicker/jquery-ui-themeroller.css',
-		'js/jqueryPlugins/jqueryDatepicker/18n/ui.datepicker-fr.js',
-		'js/jqueryPlugins/jqueryDatepicker/jquery-ui-personalized-1.6b.packed.js'
+		'js/jquery-ui-i18n.js',
+		#- ~ 'js/jqueryPlugins/jqueryDatepicker/18n/ui.datepicker-fr.js',
+		#- ~ 'js/jqueryPlugins/jqueryDatepicker/jquery-ui-personalized-1.6b.packed.js'
 	);
-	public $requiredPlugins = array('jquery');
+	public $requiredPlugins = array('jqueryUI');
+
+	function init(){
+		$lg=langManager::getCurrentLang();
+		$this->_js_script("
+			$.datepicker.setDefaults($.extend({
+					showOn:'both',
+					buttonText:'".($lg==='fr'?'Choisir une date':'pick a date')."',
+					buttonImage: '".ELEMENTS_URL."/icones/date.png',
+					dateFormat: 'yy-mm-dd',
+					highlightWeek: true,
+					defaultDate: +1
+				}".
+				($lg!=='en'?",$.datepicker.regional['$lg']":'')."
+			));
+		");
+	}
 
 	function datepicker($idElement,$value='',$datePickerOptionStr=null){
-
+		if( empty($datePickerOptionStr) ){
+			$datePickerOptionStr = '';
+		}elseif(is_array($datePickerOptionStr)){
+			$datePickerOptionStr = json_encode($datePickerOptionStr);
+		}
 		$this->_js_script('
-		$("#'.$idElement.'").datepicker({
-			dateFormat: "yy-mm-dd",
-			highlightWeek: true,
-			defaultDate: +1,
-			showOn:"both",
-			buttonText:"pick a date",
-			buttonImage: "'.ELEMENTS_URL.'/icones/date.png"'
-			.($datePickerOptionStr?",\n\t\t\t$datePickerOptionStr":'')
-			.'
-		});'
-	);
+		$("#'.$idElement.'").datepicker('.$datePickerOptionStr.')');
 
 		return 	'<input type="text" id="'.$idElement.'" name="'.$idElement.'" value="'.$value.'" />';
 	}
 
 	/**
-	* 
-	* @return 
+	* @return
 	* @param string $idElement name/id of the created elemenet
 	* @param string $value[optional] value of the input field
 	* @param array  $dateTimePickerOptions[optional] array(0=>'optionStringForDatePicker',1=>'optionStringForTimePicker')
@@ -63,7 +73,7 @@ class datepicker_viewHelper extends  jsPlugin_viewHelper{
 		}
 		$str = $this->datepicker("date_$idElement",$date, isset($dateTimeOptionsStrings[0])?$dateTimeOptionsStrings[0]:null);
 		$str.= $this->timepicker("time_$idElement",$time, isset($dateTimeOptionsStrings[1])?$dateTimeOptionsStrings[1]:null);
-		$str.= '<input type="text" name="'.$idElement.'" id="'.$idElement.'" value="'.$value.'" />'; 
+		$str.= '<input type="text" name="'.$idElement.'" id="'.$idElement.'" value="'.$value.'" />';
 		$this->_js_script("$('#$idElement').hide();$('#date_$idElement, #time_$idElement').change(dateTimePickerUpdate);");
 		return $str;
 	}
