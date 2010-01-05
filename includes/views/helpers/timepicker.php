894b4a47a1b8369273a9ad598f188e1efa715cca
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2010-01-05 15:22:16 +0000

    - use jquery toolkit version
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@218 f9b170eb-6430-4d69-81fd-c2540bca983d

--- includes/views/helpers/timepicker.php
+++ includes/views/helpers/timepicker.php
@@ -2,12 +2,15 @@
 class timepicker_viewHelper extends jsPlugin_viewHelper{
 	public $requiredFiles   = array(
 		'js/jqueryPlugins/jqueryMousewheel/jquery.mousewheel.js',
-		'js/jqueryPlugins/jqueryTimepicker/jquery.timepicker.js',
+		#- 'js/jqueryPlugins/jqueryTimepicker/jquery.timepicker.js',
+		'js/jquery.toolkit/src/jquery.toolkit.js',
+		'js/jqueryPlugins/jqueryTimepicker/jquery.tk.timepicker.js',
 	);
 	public $requiredPlugins = array('jquery');
-	
+
 	function timepicker($idElement,$value,$optionsString=null){
-		$this->_js_script("$('#$idElement').timepicker(".($optionsString===null?"{format:24,showSeconds:true}":$optionsString).");");
-		return "<input type=\"text\" name=\"$idElement\" value=\"$value\" id=\"$idElement\"/>";
+		$class = ( null==$optionsString)?' tk-timepicker-24-nosecs':'';
+		$this->_js_script("$('#$idElement').timepicker(".($optionsString===null?'':$optionsString).");");
+		return "<input type=\"text\" name=\"$idElement\" value=\"$value\" id=\"$idElement\" class=\"tk-timepicker$class\"/>";
 	}
 }
