89c7d0d522f468e0a5121cb2a9e84bebae3e8aad
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2010-01-05 15:25:42 +0000

    - add default dummy controller
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@221 f9b170eb-6430-4d69-81fd-c2540bca983d

---
+++ includes/controllers/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* dummy default controller to handle default request on newly installed simpleMVC
+* @class defaultController
+* @licence LGPL
+* @author jonathan gotti < jgotti at jgotti dot net >
+* @since 2009-10-23
+* @package simpleMVC
+*/
+class defaultController extends abstractController{
+	function indexAction(){ /* dummy default index action */	}
+	function errorAction(){ /* dummy default error action */	}
+}
