6613def61425413af86cdd5f3521ef4e4a0ce3e5
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2010-02-23 15:54:32 +0000

    - new codeMirror helper
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@247 f9b170eb-6430-4d69-81fd-c2540bca983d

---
+++ includes/views/helpers/codemirror.php
@@ -0,0 +1,125 @@
+<?php
+
+
+class codemirror_viewHelper extends jsPlugin_viewHelper{
+
+	public $requiredFiles   = array(
+		'js/CodeMirror-0.65/js/codemirror.js',
+		#- 'js/CodeMirror-0.65/css/docs.css'
+	);
+
+	static public $defaultOptions = array(
+		'lineNumbers' => true,
+		'tabMode' => 'shift',
+		'textWrapping'=>false,
+		'path' => '/js',
+    'height' => "550px",
+    'width' => "100%",
+		'iframeClass' => 'codeMirrorFrame'
+		//'continuousScanning' => 500
+
+
+	);
+
+	static public $languagueOpts = array(
+		'css' => array(
+			'parserfile' => "parsecss.js",
+			'stylesheet' => "css/csscolors.css",
+		),
+		'js'=> array(
+			'parserfile' =>  array("tokenizejavascript.js", "parsejavascript.js"),
+	    'stylesheet' =>  "css/jscolors.css",
+		),
+
+		'php' => array(
+			'parserfile' => array(
+				"parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js",
+				"../contrib/php/js/tokenizephp.js", "../contrib/php/js/parsephp.js",
+				"../contrib/php/js/parsephphtmlmixed.js"
+			),
+			'stylesheet' => array(
+				"css/xmlcolors.css", "css/jscolors.css",
+				"css/csscolors.css", "contrib/php/css/phpcolors.css"
+			)
+		),
+		'xml'=>array(
+			'parserfile'=> 'parsexml.js',
+			'stylesheet' => 'css/xmlcolors.css',
+		)
+	);
+
+	function init(){
+		self::$defaultOptions['path'] = ROOT_URL.'/js/CodeMirror-0.65/js/';
+		foreach(self::$languagueOpts as &$lopts){
+			if(! is_array($lopts['stylesheet'])){
+				$lopts['stylesheet'] = 	ROOT_URL.'/js/CodeMirror-0.65/'.$lopts['stylesheet'];
+			}else{
+				foreach($lopts['stylesheet'] as &$s){
+					$s = ROOT_URL.'/js/CodeMirror-0.65/'.$s;
+				}
+			}
+		}
+	}
+
+	function codemirror($name,$value=null,array $options=null){
+		if( empty($options['id']) ){
+			$id = jsPlugin_viewHelper::uniqueId();
+		}else{
+			$id = $options['id'];
+			unset($options['id']);
+		}
+		if( empty($options['class']) ){
+			$class = '';
+		}else{
+			$class = "class=\"$options[class]\"";
+			unset($options['class']);
+		}
+		$this->fromTextArea($id,$options);
+		return "<textarea id=\"$id\" name=\"$name\" $class>$value</textarea>";
+
+	}
+
+	function fromTextArea($textareaId,array $options=null){
+		if( empty($options['language'])){
+			$lang = 'js';
+		}else{
+			$lang = $options['language'];
+			unset($options['language']);
+		}
+		$options = array_merge(self::$defaultOptions,self::$languagueOpts[$lang],(array) $options);
+		$this->_js_script("CodeMirror.fromTextArea('$textareaId',".self::_optionString($options).");");
+	}
+
+}
+
+/*
+
+var editor = CodeMirror.fromTextArea('code', {
+        height: "350px",
+        parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js",
+                     "../contrib/php/js/tokenizephp.js", "../contrib/php/js/parsephp.js",
+                     "../contrib/php/js/parsephphtmlmixed.js"],
+        stylesheet: ["../../css/xmlcolors.css", "../../css/jscolors.css", "../../css/csscolors.css", "css/phpcolors.css"],
+        path: "../../js/",
+        continuousScanning: 500
+      });
+
+
+
+CodeMirror.fromTextArea(elmt.get(0), {
+						parserfile: "parsecss.js",
+						stylesheet: "css/csscolors.css",
+						path: "js/",
+						lineNumbers:true,
+						tabMode:'shift',
+						textWrapping:false,
+						saveFunction:function(){
+							tkWidget.computeStyle('inject');
+							return false;
+						},
+						initCallback:function(){
+							tkWidget._applyOpts('content');
+							$(tkWidget.editor.win.document).keypress(function(e){tkWidget.shortCutKey(e)});
+							tkWidget.editor.focus();
+						}
+					}*/
