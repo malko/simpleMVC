40ded326d0699f87f3d48db42a8bfc5ace3c9b5a
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2010-04-01 22:08:01 +0000

    - latest version of codeMirror and dryCss
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@275 f9b170eb-6430-4d69-81fd-c2540bca983d

--- includes/views/helpers/codemirror.php
+++ includes/views/helpers/codemirror.php
@@ -4,7 +4,7 @@
 class codemirror_viewHelper extends jsPlugin_viewHelper{
 
 	public $requiredFiles   = array(
-		'js/CodeMirror-0.65/js/codemirror.js',
+		'js/CodeMirror-0.66/js/codemirror.js',
 		#- 'js/CodeMirror-0.65/css/docs.css'
 	);
 
@@ -49,13 +49,13 @@ class codemirror_viewHelper extends jsPlugin_viewHelper{
 	);
 
 	function init(){
-		self::$defaultOptions['path'] = ROOT_URL.'/js/CodeMirror-0.65/js/';
+		self::$defaultOptions['path'] = ROOT_URL.'/js/CodeMirror-0.66/js/';
 		foreach(self::$languagueOpts as &$lopts){
 			if(! is_array($lopts['stylesheet'])){
-				$lopts['stylesheet'] = 	ROOT_URL.'/js/CodeMirror-0.65/'.$lopts['stylesheet'];
+				$lopts['stylesheet'] = 	ROOT_URL.'/js/CodeMirror-0.66/'.$lopts['stylesheet'];
 			}else{
 				foreach($lopts['stylesheet'] as &$s){
-					$s = ROOT_URL.'/js/CodeMirror-0.65/'.$s;
+					$s = ROOT_URL.'/js/CodeMirror-0.66/'.$s;
 				}
 			}
 		}
