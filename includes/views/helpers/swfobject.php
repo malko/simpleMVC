9fcc051369b02347e3ff7fb66f89fa01672be58c
malko <malko@f9b170eb-6430-4d69-81fd-c2540bca983d>
2011-03-18 01:16:19 +0000

    - rewrite and completion of swfobject + new swfobject library
    git-svn-id: http://svn.jgotti.net/simpleMVC/trunk@372 f9b170eb-6430-4d69-81fd-c2540bca983d

--- includes/views/helpers/swfobject.php
+++ includes/views/helpers/swfobject.php
@@ -2,15 +2,32 @@
 
 class swfobject_viewHelper extends jsPlugin_viewHelper{
 
-	public $requiredFiles   = array('js/swfobject.js');
+	public $requiredFiles   = array('js/swfobject/swfobject.js');
 	public $requiredPlugins = array();
 
-	function swfobject($swfUrl,$name='myswf',$w='100%',$h='100%',$v=8,$bg='#fff',$defaultContent="",$wmode=""){
+	static $dfltOptions = array(
+		'flashvars' => array(),
+		'params' => array(
+			'quality'=> 'high',
+			'wmode'  => 'window',
+			'bgcolor'=> '#fff',
+			'allowFullscreen' => true
+		),
+		'attributes' => array()
+	);
+
+	/**
+	*
+	*/
+	function swfobject($swfUrl,$name='myswf',$w='100%',$h='100%',$version='8',$defaultContent="",array $options=array()){
+		$options = array_merge_recursive(self::$dfltOptions,$options);
+		$_opts = new stdClass();
+		foreach($options as $k=>$v){
+			$_opts->$k = empty($v)?new stdClass():$v;
+		}
 		$this->_js_script("
-		var fo = new SwfObject('$swfUrl', '$name', '$w', '$h', '$v', '$bg');
-			fo.addParam('quality', 'high');".($wmode?"fo.addParam('wmode', '$wmode');":'')."
-			//fo.setAttribute('redirectUrl', 'noflash.php');
-			fo.write('$name');
+		var ".$name."_opts = ".json_encode($_opts).";
+		swfobject.embedSWF('$swfUrl', '$name', '$w', '$h', '$version','".ROOT_URL."/js/swfobject/expressInstall.swf', ".$name."_opts.flashvars, ".$name."_opts.params, ".$name."_opts.attributes);
 		");
 		return "<div id=\"$name\">$defaultContent</div>";
 	}
